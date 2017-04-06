6f03228d65a68bc3673dee1c8031bc28b231f377
Vultren <vewalden@gmail.com>
2017-04-05 22:04:04 -0400

    adding icons

--- index.php
+++ index.php
@@ -701,7 +701,7 @@
 			</div>
 		</div>
 		<span style="font-family:'Exo';visibility:hidden;">.</span><span style="font-family:'Ubuntu';visibility:hidden;">.</span>
-		<div id="footer"><p>© 2014-2016 TBES, all rights belong to their respective owners. Last updated: Jul 29 2016</p></div>
+		<div id="footer"><p>© 2014-2017 TBES, Sanya.moe, all rights belong to their respective owners. Last updated: Apr 05 2017</p></div>
 		<div style="visibility:hidden; overflow-y: hidden; height:0;" id="icondump">
 			<img src="bg.jpg" id="bg"></img>
 			<img src="furniture/chest/000.png" id="r0"></img>
